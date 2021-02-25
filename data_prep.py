0e46fff6ea78d36a60ce3b71ffe7d967e73c6d03
samanthahoch <hoch.s@husky.neu.edu>
2021-02-25 18:37:17 -0500

    Adding plotting

--- data_prep.py
+++ data_prep.py
@@ -3,14 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import descartes
 import geopandas as gpd
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point, Polygon, shape
 
 # data source: https://s3.amazonaws.com/hubway-data/index.html
 blue_bikes_csv = pd.read_csv("data/201501-hubway-tripdata.csv")
 
-geometry = [Point(xy) for xy in zip(blue_bikes_csv['start station longitude'], blue_bikes_csv['start station latitude'])]
-geo_df = gpd.GeoDataFrame(blue_bikes_csv, crs=4326, geometry=geometry)
-geo_df.head()
+start_points = [Point(xy) for xy in zip(blue_bikes_csv['start station longitude'], blue_bikes_csv['start station latitude'])]
+start_df = gpd.GeoDataFrame(blue_bikes_csv, crs=4326, geometry=start_points)
 
-fig, ax = plt.subplots(figsize=(15, 15))
-geo_df.plot(ax=ax, markersize=20, color="blue", marker="^", label="start")
+end_points = [Point(xy) for xy in zip(blue_bikes_csv['start station longitude'], blue_bikes_csv['start station latitude'])]
+points = zip(start_points, end_points)
+data = {'type': 'MultiLineString', 'coordinates': points}
+data_shape = shape(data)
+
+
+end_df = gpd.GeoDataFrame(blue_bikes_csv, crs=4326, geometry=end_points)
+
+fig, ax = plt.subplots(figsize=(7, 7))
+start_df.plot(ax=ax, markersize=20, color="blue", marker="^", label="start")
+start_df.plot(ax=ax, markersize=20, color="red", marker="^", label="end")
+plt.show()
